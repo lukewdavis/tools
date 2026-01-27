f31c26d5836e1e9ab2346bbf57aa0d533972e7f7
Luke Davis <99206365+lukewdavis@users.noreply.github.com>
2026-01-27 15:45:06 -0500

    Enhance script with additional downloads and cleanup
    Added additional URLs for downloading related files and implemented cleanup for temporary folders.

--- BRAFB/start.ps1
+++ BRAFB/start.ps1
@@ -10,10 +10,31 @@ if (-not ([Security.Principal.WindowsPrincipal] `
 }
 
 $Url = "https://github.com/PSAppDeployToolkit/PSAppDeployToolkit/releases/download/3.8.4/PSAppDeployToolkit_v3.8.4.zip"
+$shctURL = "https://tools.thelukedavis.com/BRAFB/BRAFB%20Verona%20VPN.lnk"
+$deployURL = "https://tools.thelukedavis.com/BRAFB/Deploy-SonicWallGVC.ps1"
+$installerURL = "https://tools.thelukedavis.com/BRAFB/GVCInstall64.msi"
+$configURL = "https://tools.thelukedavis.com/BRAFB/default.rcf"
+$startURL = "https://tools.thelukedavis.com/BRAFB/start.ps1"
+
 $Dest = "C:\temp\SonicWallGVC\PSAppDeployToolkit_v3.8.4.zip"
+$shctDest = "C:\temp\SonicWallGVC\BRAFB Verona VPN.lnk"
+$installerDest = "C:\temp\SonicWallGVC\GVCInstall64.msi"
+$deployDest = "C:\temp\SonicWallGVC\Deploy-SonicWallGVC.ps1"
+$configDest = "C:\temp\SonicWallGVC\default.rcf"
+$startDest = "C:\temp\SonicWallGVC\start.ps1"
 
 New-Item -ItemType Directory -Path C:\temp\SonicWallGVC -Force | Out-Null
 Invoke-WebRequest -Uri $Url -OutFile $Dest
+
+Invoke-WebRequest -Uri $shctURL -OutFile $shctDest
+
+Invoke-WebRequest -Uri $deployURL -OutFile $deployDest
+
+Invoke-WebRequest -Uri $installerURL -OutFile $installerDest
+
+Invoke-WebRequest -Uri $configURL -OutFile $configDest
+
+Invoke-WebRequest -Uri $startURL -OutFile $startDest
 
 Unblock-File -Path C:\temp\SonicWallGVC\PSAppDeployToolkit_v3.8.4.zip
 
@@ -32,3 +53,7 @@ Copy-Item -Path "C:\temp\SonicWallGVC\GVCInstall64.msi" -Destination "C:\temp\So
 Copy-Item -Path "C:\temp\SonicWallGVC\default.rcf" -Destination "C:\Program Files\SonicWall\Global VPN Client"
 
 Copy-Item -Path "C:\temp\SonicWallGVC\BRAFB Verona VPN.lnk" -Destination "C:\Users\Public\Desktop"
+
+# Cleanup temporary folders
+if (Test-Path "C:\temp\SonicWallGVC") { Remove-Item -Path "C:\temp\SonicWallGVC" -Recurse -Force -ErrorAction SilentlyContinue }
+if (Test-Path "C:\temp\PADT") { Remove-Item -Path "C:\temp\PADT" -Recurse -Force -ErrorAction SilentlyContinue }
