9f56747331d1cd5791f3acf768bdfa47eff7c18a
Luke Davis <99206365+lukewdavis@users.noreply.github.com>
2026-01-29 10:38:38 -0500

    Update VPN script with new file paths and URLs

--- BRAFB/start.ps1
+++ BRAFB/start.ps1
@@ -10,14 +10,14 @@ if (-not ([Security.Principal.WindowsPrincipal] `
 }
 
 $Url = "https://github.com/PSAppDeployToolkit/PSAppDeployToolkit/releases/download/3.8.4/PSAppDeployToolkit_v3.8.4.zip"
-$shctURL = "https://tools.thelukedavis.com/BRAFB/BRAFB%20Verona%20VPN.lnk"
+$shctURL = "https://tools.thelukedavis.com/BRAFB/Remote%Work%20VPN.lnk"
 $deployURL = "https://tools.thelukedavis.com/BRAFB/Deploy-SonicWallGVC.ps1"
 $installerURL = "https://tools.thelukedavis.com/BRAFB/GVCInstall64.msi"
 $configURL = "https://tools.thelukedavis.com/BRAFB/default.rcf"
 $startURL = "https://tools.thelukedavis.com/BRAFB/start.ps1"
 
 $Dest = "C:\temp\SonicWallGVC\PSAppDeployToolkit_v3.8.4.zip"
-$shctDest = "C:\temp\SonicWallGVC\BRAFB Verona VPN.lnk"
+$shctDest = "C:\temp\SonicWallGVC\Remote Work VPN.lnk"
 $installerDest = "C:\temp\SonicWallGVC\GVCInstall64.msi"
 $deployDest = "C:\temp\SonicWallGVC\Deploy-SonicWallGVC.ps1"
 $configDest = "C:\temp\SonicWallGVC\default.rcf"
@@ -52,8 +52,9 @@ Copy-Item -Path "C:\temp\SonicWallGVC\GVCInstall64.msi" -Destination "C:\temp\So
 
 Copy-Item -Path "C:\temp\SonicWallGVC\default.rcf" -Destination "C:\Program Files\SonicWall\Global VPN Client"
 
-Copy-Item -Path "C:\temp\SonicWallGVC\BRAFB Verona VPN.lnk" -Destination "C:\Users\Public\Desktop"
+Copy-Item -Path "C:\temp\SonicWallGVC\Remote Work VPN.lnk" -Destination "C:\Users\Public\Desktop"
 
 # Cleanup temporary folders
 if (Test-Path "C:\temp\SonicWallGVC") { Remove-Item -Path "C:\temp\SonicWallGVC" -Recurse -Force -ErrorAction SilentlyContinue }
 if (Test-Path "C:\temp\PADT") { Remove-Item -Path "C:\temp\PADT" -Recurse -Force -ErrorAction SilentlyContinue }
+
